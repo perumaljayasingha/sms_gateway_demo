e0ac02c0ebf0cd7f3a02f8524b3092e4fcd8934d
Jayasingh <jayasinghperumal@gmil.com>
2025-01-15 20:32:08 +0530

    implement the swagger

--- src/test/java/com/smsc/smsc_singup_login_api/SmscSingupLoginApiApplicationTests.java
+++ src/test/java/com/smsc/smsc_singup_login_api/SmscSingupLoginApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.sms_singup_login_api;
+package com.smsc.smsc_singup_login_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SmsSingupLoginApiApplicationTests {
+class SmscSingupLoginApiApplicationTests {
 
 	@Test
 	void contextLoads() {
